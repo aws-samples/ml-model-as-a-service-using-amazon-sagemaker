b99b8a8dad40aa832abd24254a07d559c21b5d31
Ujwal Bukka <ujwbukka@amazon.com>
2023-09-13 12:04:54 -0400

    updated git push commands

--- server/deploy-tenant-pipeline.sh
+++ server/deploy-tenant-pipeline.sh
@@ -12,16 +12,13 @@ if ! aws codecommit get-repository --repository-name ml-saas-workshop; then
   echo "ml-saas-workshop codecommit repo is not present, will create one now"
   CREATE_REPO=$(aws codecommit create-repository --repository-name ml-saas-workshop --repository-description "ML saas workshop repository")
   echo "$CREATE_REPO"
-  REPO_URL="codecommit::${REGION}://ml-saas-workshop"
-  if ! git remote add cc "$REPO_URL"; then
-    echo "Setting url to remote cc"
-    git remote set-url cc "$REPO_URL"
-  fi
-  git push --set-upstream cc main
-  git add -u .
-  git commit -m 'Commit changes'
-  git push
+fi  
+REPO_URL="codecommit::${REGION}://ml-saas-workshop"
+if ! git remote add cc "$REPO_URL"; then
+  echo "Setting url to remote cc"
+  git remote set-url cc "$REPO_URL"
 fi
+git push cc "$(git branch --show-current)":main
 
 # enable yarn
 corepack enable || npm install --global yarn
