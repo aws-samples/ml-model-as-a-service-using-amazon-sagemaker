54722ac2f449cae3600e8013942e6bf9b2c6db78
Ujwal Bukka <ujwbukka@amazon.com>
2023-09-22 16:32:26 -0400

    Added code to upload initial basic tier model

--- setup/create_basic_tier_sagemaker_endpoint.py
+++ setup/create_basic_tier_sagemaker_endpoint.py
@@ -2,17 +2,19 @@ import boto3
 import sagemaker
 
 def create_sagemaker_endpoint():
+    model_name='GenericModel'
+    endpoint_config_name = f"EndpointConfig-{model_name}"
+    endpoint_name = f"Endpoint-{model_name}"
+    model_data_key = 'model_artifacts/basic_tier/output/model.tar.gz'
+
     try:
         client = boto3.client('sagemaker')
         region = boto3.Session().region_name
         role = sagemaker.get_execution_role()
         account_number = boto3.client('sts').get_caller_identity().get('Account')
-
-        model_name='GenericModel'
-        endpoint_config_name = f"EndpointConfig-{model_name}"
-        endpoint_name = f"Endpoint-{model_name}"
         generic_model_data_bucket_name = f"sagemaker-mlaas-pooled-{region}-{account_number}"
-        model_data_folder = 'basic_tier'
+
+        upload_initial_model(generic_model_data_bucket_name, model_data_key)
 
         # get image URI
         image_uri = sagemaker.image_uris.retrieve(
@@ -28,7 +30,7 @@ def create_sagemaker_endpoint():
                                     ModelName=model_name,
                                     PrimaryContainer={
                                         'Image': image_uri,
-                                        'ModelDataUrl': f"s3://{generic_model_data_bucket_name}/model_artifacts/{model_data_folder}/output/model.tar.gz",
+                                        'ModelDataUrl': f"s3://{generic_model_data_bucket_name}/{model_data_key}",
                                         'Environment': {}
                                     },
                                     ExecutionRoleArn=role
@@ -68,7 +70,11 @@ def create_sagemaker_endpoint():
         print('Error occured while creating sagemaker endpoint')
         raise Exception('Error occured while creating sagemaker endpoint', e)     
 
-    
+
+def upload_initial_model(bucket_name, key):
+    s3 = boto3.resource('s3')
+    s3.Bucket(bucket_name).upload_file('model.tar.gz', key)
+
 
 if __name__ == '__main__':
     
