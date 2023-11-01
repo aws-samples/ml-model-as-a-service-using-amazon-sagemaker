3ec442432ca82d1c507a3efe39e69f19fcb7361b
Ujwal Bukka <ujwbukka@amazon.com>
2023-11-01 15:43:17 -0400

    Updated pipeline objective hyperparameter to be logistic and recreated model artifacts

--- notebook/pipeline.py
+++ notebook/pipeline.py
@@ -237,7 +237,7 @@ def get_pipeline(
         role=role,
     )
     xgb_train.set_hyperparameters(
-        objective="reg:linear",
+        objective="binary:logistic",
         num_round=50,
         max_depth=5,
         eta=0.2,
