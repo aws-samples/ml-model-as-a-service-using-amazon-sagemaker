314589cedacd4817c6c9687d8fa29f8e83609f57
Ujwal Bukka <ujwbukka@amazon.com>
2023-11-01 20:11:10 -0400

    Updated script to pass username

--- setup/create-admin-user.sh
+++ setup/create-admin-user.sh
@@ -1,30 +1,37 @@
 #!/bin/bash -e
 
+USERNAME=$1
+if [[ -z "$USERNAME" ]]; then
+    echo "No user name is provided, default username: admin-user will be used to create SaaS admin user" 
+    USERNAME="admin-user"
+fi
+
+
 ADMIN_USERPOOLID=$(aws cloudformation describe-stacks --stack-name mlaas --query "Stacks[0].Outputs[?OutputKey=='CognitoOperationUsersUserPoolId'].OutputValue" --output text)
 ADMIN_USER_GROUP_NAME=$(aws cloudformation describe-stacks --stack-name mlaas --query "Stacks[0].Outputs[?OutputKey=='CognitoAdminUserGroupName'].OutputValue" --output text)
 ADMIN_SITE_URL=$(aws cloudformation describe-stacks --stack-name mlaas --query "Stacks[0].Outputs[?OutputKey=='AdminAppSite'].OutputValue" --output text)
 
-# Create admin-user in OperationUsers userpool with given input email address
+# Create admin user in OperationUsers userpool with given input email address
 CREATE_ADMIN_USER=$(aws cognito-idp admin-create-user \
 --user-pool-id $ADMIN_USERPOOLID \
---username admin-user \
+--username $USERNAME \
 --user-attributes Name=email,Value=admin-user@example.com Name=phone_number,Value="+11234567890" Name="custom:userRole",Value="SystemAdmin" Name="custom:tenantId",Value="system_admins" \
 --desired-delivery-mediums EMAIL)
 
 # echo "$CREATE_ADMIN_USER"
 
-# Add admin-user to admin user group
+# Add admin user to admin user group
 ADD_ADMIN_USER_TO_GROUP=$(aws cognito-idp admin-add-user-to-group \
 --user-pool-id $ADMIN_USERPOOLID \
---username admin-user \
+--username $USERNAME \
 --group-name $ADMIN_USER_GROUP_NAME)
 
 # Setting admin-user password
 SET_ADMIN_USER_PASSWORD=$(aws cognito-idp admin-set-user-password \
 --user-pool-id $ADMIN_USERPOOLID \
---username admin-user \
+--username $USERNAME \
 --password 'Mlaa$1234' \
 --permanent)
 
 echo "Admin user created successfully."
-echo "Please use admin username: admin-user and password: Mlaa\$1234 to login to Control Plane admin site URL: https://$ADMIN_SITE_URL"
+echo "Please use admin username: $USERNAME and password: Mlaa\$1234 to login to Control Plane admin site URL: https://$ADMIN_SITE_URL"
