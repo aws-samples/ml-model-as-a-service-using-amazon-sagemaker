077dd0c8f5f2d2060ebe20b003177d17d2ab625a
Ujwal Bukka <ujwbukka@amazon.com>
2024-01-03 15:51:44 -0500

    Updated code to get region

--- server/deploy-tenant-pipeline.sh
+++ server/deploy-tenant-pipeline.sh
@@ -4,7 +4,8 @@ echo "Deploy tenant pipeline"
 REGION="$AWS_REGION"
   if [ -z "$REGION" ]; then
     # AWS_REGION is empty, try to get region using aws configure
-    REGION=$(aws configure get region)
+    TOKEN=$(curl -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 5")
+    REGION=$(curl -H "X-aws-ec2-metadata-token: $TOKEN" -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/\(.*\)[a-z]/\1/')
   fi
 echo "Region: $REGION"
 
